52745737417f71915e64bdcb7b17dd5fa6a9abe2
mitya_me <dmitrymefodyev@gmail.com>
2022-02-09 23:23:15 +0300

    Update styles and add custom text components

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,27 +1,45 @@
 import React from 'react'
-import { View, Text, KeyboardAvoidingView, StyleSheet, ScrollView, useWindowDimensions } from 'react-native'
+import { View, StyleSheet, ScrollView, useWindowDimensions } from 'react-native'
 import CustomInput from '../components/CustomInput'
 import CustomButton from '../components/CustomButton'
 import colors from '../styles/colors'
+import CustomText from '../components/CustomText'
+import { useNavigation } from '@react-navigation/native'
 
 const LoginScreen = () => { 
-	const {height} = useWindowDimensions()
+	const { height } = useWindowDimensions()
+	const navigation = useNavigation();
+
+	const auth = true;
+
+	const onEnterPressed = () => { 
+		navigation.navigate('EmployeeList')
+	}
 
 	return (
 		<ScrollView style={styles.login}> 
-			<View style={[styles.loginHeader, {maxHeight: height * 0.66}]}>
-				<Text style={styles.loginGreeting}>👋Hello👋 </Text>
-				<Text style={styles.loginDescription}>
-				 Xavier is a member of a subspecies of humans known as mutants,
-					who are born with superhuman abilities. He is an exceptionally powerful telepath,
-					who can read and control the minds of others. To both shelter and train mutants from
-					around the world, he runs a private school in the X-Mansion in Salem Center, located
-					in Westchester County, New York.
-				</Text>
+			<View style={[styles.loginHeader, { maxHeight: height * 0.66 }]}>
+				<CustomText
+					fz={34}
+					fw='bold'
+					tAlign='center'
+					color={colors.primary}
+					padV={15}
+					text='👋Hello👋'
+				/>
+				<CustomText
+					tAlign='center'
+					color = {colors.textGray}
+					text="Xavier is a member of a subspecies of humans known as mutants,
+					   who are born with superhuman abilities. He is an exceptionally powerful telepath,
+					   who can read and control the minds of others. To both shelter and train mutants from
+					   around the world, he runs a private school in the X-Mansion in Salem Center, located
+					   in Westchester County, New York."
+				/>
 			</View>
 			<CustomInput label='Login' placeholder='Login'/>			
 			<CustomInput label='Password' placeholder='Password' />
-			<CustomButton text='Enter'/>
+			<CustomButton onPress={onEnterPressed} dis={!auth} text='Enter' bRadius={5}/>
 		</ScrollView>
 	)
 }
@@ -30,10 +48,10 @@ const styles = StyleSheet.create({
 	login: {
 		flex: 1,
 		backgroundColor: colors.bg,
-		padding: 15
+		paddingTop: '15%',
+		paddingHorizontal: 15,
 	},
 	loginHeader: {
-		paddingTop: '15%',
 		alignItems: 'center',
 		overflow: 'hidden',
 		paddingBottom: '5%',
@@ -45,12 +63,10 @@ const styles = StyleSheet.create({
 		marginBottom: 15
 	},
 	loginDescription: {
-		color: colors.textLight,
+		color: colors.textGray,
 		paddingHorizontal: 30,
 		textAlign: 'center'
 	}
-	
-
 });
 
 
