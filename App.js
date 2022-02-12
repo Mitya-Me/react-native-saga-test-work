06e59af8ec4de0702387fea2bbbe87c02d2a9b90
mitya_me <dmitrymefodyev@gmail.com>
2022-02-12 23:42:16 +0300

    Add react-redux Provider

--- App.js
+++ App.js
@@ -10,19 +10,23 @@ import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import colors from './src/styles/colors';
 import Navigation from './src/navigation'
+import { Provider } from 'react-redux';
+import store from './src/redux'
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.root}>
-      <StatusBar backgroundColor={colors.primary} />
-      <Navigation />
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={styles.root}>
+        <StatusBar backgroundColor={colors.primary} />
+        <Navigation />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   root: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: colors.bg,
   }
 })
