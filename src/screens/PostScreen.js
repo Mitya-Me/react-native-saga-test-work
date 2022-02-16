4a49c73041d5562b1d747fbdb73b6b797194c99e
mitya_me <dmitrymefodyev@gmail.com>
2022-02-16 23:11:56 +0300

    Add: navigate for back btn, logout fn, data from api

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,43 +1,39 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import colors from '../styles/colors'
 import CustomButton from '../components/CustomButton'
 import CustomText from '../components/CustomText'
+import { useDispatch } from 'react-redux' 
+import { AUTH_LOGOUT } from '../redux/reducers/authReducer/actions'
+import { useNavigation } from '@react-navigation/native'
 
-const PostScreen = ({ userName, description }) => {
-	const onPostPressed = () => { 
-		console.warn('onEmployeeItemPressed')
+
+const PostScreen = ({ route }) => {
+	const dispatch = useDispatch()
+	const navigation = useNavigation();
+	const { title, description } = route.params.post;
+
+	const onLogOutPressed = () => {
+		dispatch({ type: AUTH_LOGOUT });
 	}
 
+	const onBackPressed = () => { 
+		navigation.navigate('PostsList')
+	}
 	return (
 		<View style={styles.wrapper}>
 			<View style={styles.textBlock}>
-				<CustomText
-					fz={20}
-					fw='bold'
-					text='Xavier'
-				/>
-				<CustomText
-					text="After becoming paraplegic following a conflict against
-					the villain Lucifer in the Himalayas,[26] Xavier transformed
-					his family's ancestral home into the Xavier's School for Gifted
-					Youngsters, a sanctuary for mutants to train and learn about
-					their abilities.[27] Under his supervision, Professor X's 
-					students formed the X-Men, a super-hero group who fight for a 
-					world that hates and fears them, guided by Xavier's dream.
-					Through the years, Xavier and his X-Men entered into conflicts
-					with both humans and mutants with diverging ideologies, 
-					especially Magneto, who believed in mutant supremacy"
-				/>
+				<CustomText fz={20} fw='bold' text={title}/>
+				<CustomText text={description}/>
 			</View>
 			<View style={styles.btns}>
 				<CustomButton
-					onPress={onPostPressed}
+					onPress={onBackPressed}
 					type='secondary'
 					text='Back'
 					width='50%'
 				/>
 				<CustomButton
+					onPress={onLogOutPressed}
 					text='Logout'
 					width='50%'
 				/>
